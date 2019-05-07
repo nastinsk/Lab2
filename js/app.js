9fc4a7abdaae439fc9647ca102401d85a4da93cb
Anastasia Lebedeva <nastinsk@gmail.com>
2019-05-06 20:39:30 -0700

    initial file setup

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var car = prompt('Do you have a car?');
+
+car = 
+car.toLowerCase(); //or likesGameOfThrones. for upper case
+
+
+if(car === 'yes'){
+    alert('great');
+} else if (car === 'no'){
+    alert(':(');
+} else{
+    alert('Do you have a car?');
+}
+
+
