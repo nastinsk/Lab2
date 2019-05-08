1272b32d09fef5410e151c28a26301edad011fc3
Anastasia Lebedeva <nastinsk@gmail.com>
2019-05-07 19:32:31 -0700

    question to enter the game

--- js/app.js
+++ js/app.js
@@ -1,32 +1,43 @@
 'use strict';
 
-
 var wantPlay = prompt('Hi! welcome to my page! This is a guessing game about me. Your answer should be yes or no. Do you want to play?');
 
 wantPlay= 
 wantPlay.toLowerCase(); 
 
 
-// if(wantPlay === 'yes'){
-//     alert('great!');
-// } else if (car === 'no'){
-//     alert('see you next time!');
-// } else{
-    
-while (wantPlay !== 'yes' && 'no'){
-    wantPlay;
+
+while (wantPlay !== 'yes' &&  wantPlay !=='no') {
+    wantPlay = prompt('Hi! Welcome to my page! This is a guessing game about me. Your answer should be yes or no. Do you want to play?');
 }
 
 if (wantPlay === 'yes'){
-        alert('great!');
+    alert('great!');
 }
-
-else if (wantPlay === 'no'){
+else {
     alert('see you next time!');
+    throw('see you next time!');
+    
 }
-// else { while (wantPlay !== 'yes' && 'no'){
-//      wantPlay2 = prompt('Do you want to play?');
-// }
 
-// }
- 
+var fullName = prompt('Is my full name Anastasia?');
+
+fullName= 
+fullName.toLowerCase(); 
+
+
+var origin = Prompt('Was I born in Ukraine?');
+
+origin = 
+origin.toLowerCase(); 
+
+
+var likeCoding = prompt('Do i like coding?');
+
+likeCoding = 
+likeCoding.toLowerCase(); 
+
+var learnCode = prompt('Would you like to learn how to code too?');
+
+learnCode = 
+lernCode.toLowerCase();
