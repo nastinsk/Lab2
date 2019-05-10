9db6996d89551046424160040be1ff8d1be1b86e
Anastasia Lebedeva <nastinsk@gmail.com>
2019-05-09 20:11:58 -0700

    first array of object added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,120 @@
 'use strict';
 
-// I used a lot of repetitive code in this file, in future I am planning to make it into the function.
+//collecting the users name
+var userName = prompt('Hi! welcome to my page! This is a guessing game about me. What is your name?');
+console.log ('User name is ' + userName);
+
+//loop to get name from user
+while (userName === '') {
+  userName = prompt('Hi! welcome to my page! This is a guessing game about me. What is your name?');
+  console.log ('User name is ' + userName); 
+}
+  
+//array created to store questions from program
+var gameResponds = [
+  {q: 'Nice to meet you ' + userName + ' ! Do you want to play my game?',
+   y: 'Great!',
+   n: 'see you next time!'
+  },
+  {q: 'Is my full name Anastasia?',
+   y: 'Yes, you are right!',
+   n: 'Wrong. My name is Anastasia!'
+  },
+  ''];
+
+
+
+//invitation to play a game, with choice to quit
+var wantPlay = prompt(gameResponds[0].q);
+console.log ('Does user want to play?:' + wantPlay);
+
+while (wantPlay !== 'yes' && wantPlay !=='no' && wantPlay !=='y' && wantPlay !=='n') {
+  wantPlay = prompt(gameResponds[0].q);
+}
+  
+if (wantPlay === 'yes' || wantPlay === 'y'){
+  alert(gameResponds[0].y);
+}
+else if (wantPlay === 'no' || wantPlay === 'n') {
+  alert(gameResponds[0].n);
+  throw(gameResponds[0].n);
+}
+
+
+
+//question about my name, user must choose 'yes', 'no', 'y', 'n'. If user choses something different while loop repeat the question,
+//untill valid answer won't be received.
+  
+var fullName = prompt(gameResponds[1].q);
+  
+fullName = fullName.toLowerCase();
+console.log ('Does user think my name is Anastasia?: ' + fullName);
+  
+while (fullName !== 'yes' &&  fullName !=='no' && fullName !=='y' && fullName !=='n') {
+  fullName = prompt(gameResponds[1].q);
+}
+  
+if (fullName === 'yes' || fullName === 'y'){
+  alert(gameResponds[1].y);
+}
+  
+else if (fullName === 'no' || fullName === 'n') {
+  alert(gameResponds[1].n);
+}
+  
+  
+
+  
+//question about my myOrigin, user must choose 'yes', 'no', 'y', 'n'. If user choses something different while loop repeat the question,
+//untill valid answer won't be received.
+  
+var myOrigin = prompt('Was I born in Ukraine?');
+  
+myOrigin = myOrigin.toLowerCase();
+  
+console.log ('Does user think i was born in Ukraine?: ' + myOrigin);
+  
+while (myOrigin !== 'yes' &&  myOrigin !=='no' && myOrigin !== 'y' && myOrigin !== 'n') {
+  myOrigin = prompt('Was I born in Ukraine?');
+}
+  
+if (myOrigin === 'yes' || myOrigin === 'y' ){
+  alert('No. I am from Russia.');
+}
+else if (myOrigin === 'no' || myOrigin === 'n') {
+  alert('Yes! You are right, I was born in Russia.');
+}
+  
+  
+  
+//question about like I code or not, user must -//-
+  
+var likeCode = prompt('Do I like code?');
+  
+likeCode =
+  likeCode.toLowerCase();
+console.log ('Does user think that I like code? ' + likeCode);
+  
+while (likeCode !== 'yes' &&  likeCode !=='no' && likeCode !== 'y' && likeCode !== 'n') {
+  likeCode = prompt('Do I like code?');
+}
+  
+if (likeCode === 'yes' || likeCode === 'y'){
+  alert('Yes! I love coding!');
+}
+else if (likeCode === 'no' || likeCode === 'n' ) {
+  alert('Wrong. I love coding!');
+}
+  
+
+
+
+
+
+
+
+
+/* // I used a lot of repetitive code in this file, in future I am planning to make it into the function.
 
 //welcome to my page question, if user doesn't want to play game he gets to the page AboutMe after he presses 'no' or 'n'
 var wantPlay = prompt('Hi! welcome to my page! This is a guessing game about me. Your answer should be yes or no. Do you want to play?');
@@ -103,4 +217,4 @@ if (wantLearnCode === 'yes' || wantLearnCode === 'y'){
 }
 else if (wantLearnCode === 'no' || wantLearnCode === 'n'){
   alert('See you next time!');
-}
+} */
